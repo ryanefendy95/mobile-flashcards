3b5762ff23b101e5a8258b61616b08cafb37b237
Ryan Efendy <ryanefendy95@gmail.com>
2018-11-21 00:53:46 -0800

    FEAT: add all screens, add navigations, replace package.lock.json -> yarn.lock :fire::fire::fire:

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 export default class App extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+    });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <TabsContainer />;
   }
 }
+
+const Hello = () => (
+  <View>
+    <Text>Hello!</Text>
+  </View>
+);
+
+const Goodbye = () => (
+  <View>
+    <Text>Goodbye!</Text>
+  </View>
+);
+
+const Tabs = createBottomTabNavigator({
+  Hello: {
+    screen: Hello
+  },
+  Goodbye: {
+    screen: Goodbye
+  }
+});
+
+const TabsContainer = createAppContainer(Tabs);
 
 const styles = StyleSheet.create({
   container: {
