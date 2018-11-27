b0df48d940a52370c4f6c6265c33b48aa77f837b
Ryan Efendy <ryanefendy95@gmail.com>
2018-11-27 08:09:39 -0800

    FIX: bottom tab navigation

--- navigations/MainTabNavigator.js
+++ navigations/MainTabNavigator.js
@@ -1,15 +1,47 @@
+import React, { Component } from 'react';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 import DeckListScreen from '../screens/DeckListScreen';
 import NewDeckScreen from '../screens/NewDeckScreen';
 
-const Tabs = createBottomTabNavigator({
-  DeckList: {
-    screen: DeckListScreen
+const Tabs = createBottomTabNavigator(
+  {
+    DeckList: {
+      screen: DeckListScreen,
+      navigationOptions: () => ({
+        title: `Decks`
+      })
+    },
+    NewDeck: {
+      screen: NewDeckScreen,
+      navigationOptions: () => ({
+        title: `Add Deck`
+      })
+    }
   },
-  NewDeck: {
-    screen: NewDeckScreen
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      title: 'titleName',
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'DeckList') {
+          iconName = `ios-apps`;
+        } else if (routeName === 'NewDeck') {
+          iconName = `md-add-circle${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray'
+    }
   }
-});
+);
 
 const TabsContainer = createAppContainer(Tabs);
 
