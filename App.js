a2175c7114d9b9559e51ebf9cdf7e7380d4c196b
Ryan Efendy <ryanefendy95@gmail.com>
2018-11-28 09:21:49 -0800

    FEAT: Added AsynStorage, reactotron FIX: remove package.lock.json

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigations/AppNavigator';
+// import './utils/ReactotronConfig';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  // componentDidMount() {
+  //   Reactotron.connect();
+  // }
+
   state = {
     isLoadingComplete: false
   };
--- navigations/MainTabNavigator.js
+++ navigations/MainTabNavigator.js
@@ -10,10 +10,10 @@ import NewQuestionScreen from '../screens/NewQuestionScreen';
 import QuizScreen from '../screens/QuizScreen';
 
 const DeckListStack = createStackNavigator({
-  Quiz: QuizScreen,
   DeckList: DeckListScreen,
   Deck: DeckScreen,
-  NewQuestion: NewQuestionScreen // add card
+  NewQuestion: NewQuestionScreen, // add card
+  Quiz: QuizScreen
 });
 
 DeckListStack.navigationOptions = {
